f032a75b03eedaf1f564faf75bcf21bf90338070
Akash B  G <akash.bg@corp.rapyder.com>
2023-03-25 22:54:08 +0530

    Ui Development v1.1 get data validation from backend

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,9 +2,28 @@ import React from 'react';
 import '../../App.css';
 import './services.css'
 import './Services'
-import {upload, buttoncliked} from './functions'
+import {upload, fetchData} from './functions'
+
+var firstName, lastName, idNumber, birthDate;
+var userInputData = {};
 
 export default function Services() {
+
+  const useButtoncliked = async () => {
+    firstName=document.querySelector("#firstName").value;
+    lastName=document.querySelector("#lastName").value;
+    idNumber=document.querySelector("#idNumber").value;
+    birthDate=document.querySelector("#birthDate").value;
+    userInputData = { 
+      first_name: firstName,
+      last_name: lastName,
+      middle_name: "",
+      id_number: idNumber,
+      date_of_birth: birthDate,
+    };
+    const output = await fetchData(userInputData)
+    console.log(output)
+  }
 
   return <div className='sign-up'>
   <div className="Servicebox">
@@ -28,10 +47,10 @@ export default function Services() {
     </select>
   </div>
   <div className='FileSubmit'>
-  <input type="file" id="myFile" onChange={upload} name="filename" accept="image/png, image/jpeg, image/jpg" />
+  <input type="file" id="myFile" onChange={upload} name="filename" accept="image/png, image/jpeg, image/jpg"/>
   </div>
   <div className='Validate'>
-  <button onClick={buttoncliked} id="validate" className='btnLogin'>Validate</button>
+  <button onClick={useButtoncliked} id="validate" className='btnLogin'>Validate</button>
   </div>
   </div>
 </div>;
--- src/components/pages/functions.js
+++ src/components/pages/functions.js
@@ -1,17 +1,12 @@
 import { Upload } from "@aws-sdk/lib-storage";
 import { S3Client} from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from 'uuid';
-import { TextractClient, AnalyzeIDCommand } from "@aws-sdk/client-textract";
-import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
 import axios from 'axios';
-import { SignatureV4 } from '@aws-sdk/signature-v4';
-import { Sha256 } from '@aws-crypto/sha256-js';
 
-var firstName, lastName, idNumber, birthDate, fileName;
-var userInputData = new Object();
+var fileName;
 var s3BucketName = process.env.REACT_APP_S3_BUCKET_NAME;
 var awsRegion = process.env.REACT_APP_AWS_REGION;
-var awsLambda = process.env.REACT_APP_LAMBDA_FUNCTION_NAME;
+var backend_host = process.env.REACT_APP_BACKEND_HOST_NAME;
 var credentials = {
   accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID, 
   secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
@@ -19,12 +14,12 @@ var credentials = {
 
 export const upload = (file)=>{
   fileName = uuidv4()+"-"+file.target.files[0].name;
-  var file = file.target.files[0];
-  if (file.size > 10e6) {
+  var uploadedFile = file.target.files[0];
+  if (uploadedFile.size > 10e6) {
     window.alert("Please upload a file smaller than 10 MB");
     return false;
   }
-  const target = {Bucket: s3BucketName, Key:fileName, Body:file};
+  const target = {Bucket: s3BucketName, Key:fileName, Body:uploadedFile};
   try {
     const Uploads3 = new Upload({
       client: new S3Client({region:awsRegion, credentials:credentials}),
@@ -41,55 +36,11 @@ export const upload = (file)=>{
   }
 }
 
-export const buttoncliked = () => {
-    firstName=document.querySelector("#firstName").value;
-    lastName=document.querySelector("#lastName").value;
-    idNumber=document.querySelector("#idNumber").value;
-    birthDate=document.querySelector("#birthDate").value;
-    userInputData = { 
-      "FIRST_NAME": firstName,
-      "LAST_NAME": lastName,
-      "MIDDLE_NAME": "",
-      "DOCUMENT_NUMBER": idNumber,
-      "DATE_OF_BIRTH": birthDate,
-      "UPLOADED_FILE": fileName
-    };
-    console.log(userInputData);
-    console.log("Sending request to AWS Lambda...");
-    var client = new LambdaClient({region:awsRegion, credentials:credentials});
-    var params = {
-      FunctionName:awsLambda,
-      Payload: [
-        {
-          "s3_busket_name": s3BucketName,
-          "image_name": fileName,
-          "FIRST_NAME": firstName,
-          "LAST_NAME": lastName,
-          "MIDDLE_NAME": "",
-          "DOCUMENT_NUMBER": idNumber,
-          "DATE_OF_BIRTH": birthDate,
-        }
-      ],
-    };
-    const command = new InvokeCommand(params);
-    const response = client.send(command);
-    console.log(response);
-    // console.log("Sending request to AWS Textract...");
-    // const client = new TextractClient({region:awsRegion, credentials:credentials});
-    // var params = {
-    //   DocumentPages:[{
-    //     "S3Object": {
-    //       "Bucket": s3BucketName,
-    //       "Name": fileName,
-    //     }
-    //   }]
-    // };
-    // const command = new AnalyzeIDCommand(params);
-    // try {
-    //   const textractResponse = client.send(command)
-    //   console.log(textractResponse)
-    // }
-    // catch (e) {
-    //   console.log(e);
-    // }
-}
+export const fetchData = async (formData) => {
+  console.log("filename: "+fileName)
+  const userData = {...formData, image_name: fileName}
+  console.log("calling backend..");
+  console.log("with data: "+userData);
+  const response = await axios.post(`http://${backend_host}/data`, userData, {headers: "Content-Type: application/json"})
+  return response.data
+  }
